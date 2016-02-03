05db68867f4ba681cc6959371d0ce737408cd03d
xfdingustc <xfdingustc@gmail.com>
2016-02-02 18:43:44 -0800

    add cart in session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+    def current_cart
+      Cart.find(session[:card_id])
+    rescue ActiveRecord::RecordNotFound
+      cart = Cart.create
+      session[:card_id] = cart.id
+      cart
+    end
 end
